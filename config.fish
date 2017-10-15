0f41226f603a0558d9022110190c8a324d0b5a1a
Takumi Kaji <ivezuki3@gmail.com>
2017-10-16 00:40:37 +0900

    Update

--- config.fish
+++ config.fish
@@ -9,6 +9,7 @@ alias bo="bundle open"
 alias bod="bundle outdated"
 alias bu="bundle update"
 alias ct='ctags --langmap=RUBY:.rb --exclude="*.js"  --exclude=".git*" -R .'
+alias dc="docker-compose"
 alias g="git"
 alias ll="ls -l"
 alias v="env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim"
@@ -43,27 +44,13 @@ function fish_prompt --description 'Write out the prompt'
 				set __git_cb ""(git branch ^/dev/null | grep \* | sed 's/* //')""
     end
 
-    switch $USER
-
-        case root toor
-
-            if not set -q __fish_prompt_cwd
-                if set -q fish_color_cwd_root
-                    set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-                else
-                    set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-                end
-            end
-
-            printf '%s@%s %s%s%s# ' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal"
-
-        case '*'
-
-            if not set -q __fish_prompt_cwd
-                set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-            end
-
-            printf '[%s] %s%s %s(%s)%s [%s] \f\r$ ' (date "+%H:%M:%S") "$__fish_prompt_cwd" "$PWD" "$__fish_color_status" "$stat" "$__fish_color_brown" "$__git_cb"
-
+    if not set -q __fish_prompt_cwd
+        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
     end
+
+    printf '[%s] %s%s %s(%s)%s [%s] \f\r$ ' (date "+%H:%M:%S") "$__fish_prompt_cwd" "$PWD" "$__fish_color_status" "$stat" "$__fish_color_brown" "$__git_cb"
 end
+
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/tkm_kj/google-cloud-sdk/path.fish.inc' ]; if type source > /dev/null; source '/Users/tkm_kj/google-cloud-sdk/path.fish.inc'; else; . '/Users/tkm_kj/google-cloud-sdk/path.fish.inc'; end; end
+eval (direnv hook fish)
