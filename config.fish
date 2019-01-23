7c1689423555f23d4b2618d30795c4c62d9f7241
Takumi Kaji <ivezuki3@gmail.com>
2019-01-23 22:30:15 +0900

    ctrl-r でpecoで過去のコマンド履歴を探せるようにした

--- config.fish
+++ config.fish
@@ -52,6 +52,22 @@ function fish_prompt --description 'Write out the prompt'
 end
 eval (direnv hook fish)
 
+function peco_select_history
+  if test (count $argv) = 0
+    set peco_flags --layout=bottom-up
+  else
+    set peco_flags --layout=bottom-up --query "$argv"
+  end
+
+  history|peco $peco_flags|read foo
+
+  if [ $foo ]
+    commandline $foo
+  else
+    commandline ''
+  end
+end
+
 function fish_user_key_bindings
     bind \cr peco_select_history
 end
